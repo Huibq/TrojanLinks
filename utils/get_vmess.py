ce2266b8efdac82cc36ea8daf39cf3ed8252be0e
Huibq <huibq520@gmail.com>
2024-06-09 12:48:50 +0800

    up

--- utils/get_vmess.py
+++ utils/get_vmess.py
@@ -20,10 +20,11 @@ from Telegram_bot import send_message
 urllib3.disable_warnings()
 
 
-def get_vmess():
+def get_vmess(ii):
+    params['area'] = ii
     base_url = vmess_api
     url = base_url + urlencode(params)
-    response = requests.get(url, headers=headers, timeout=10, verify=False)
+    response = session.get(url, verify=False, timeout=10)
     return response.text
 
 
@@ -33,15 +34,22 @@ def decrypt(cipher_text: str, key: str) -> str:
     return decrypted_text.decode('utf-8').rstrip()
 
 
-def get_address(ip):
-    url = f'https://ip125.com/api/{ip}?lang=zh-CN'
-    head = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0',
-        'Cookie': '_ga=GA1.2.348223593.1668760697; _ga_XYJPKQNDKR=GS1.1.1669809730.4.1.1669809752.0.0.0; __gads=ID=e9cb6076c0188403-228d0f367edf00b9:T=1683097828:RT=1701660864:S=ALNI_MZoNQcRpP-66ZZidp6BAlct92mbOw; __gpi=UID=00000c011afd3f29:T=1683097828:RT=1701660864:S=ALNI_MZSTguCSNwyc6d4WgMIcm7m-Xepvg'
-    }
-    country_info = requests.get(url, headers=head).json()
-    add = country_info['country'] + country_info['city']
-    return add
+def getlist():
+    node_list = []
+    new_params = {'level': 2}
+    del params['area']
+    new_params.update(params)
+    base_url = vmess_api2
+    url = base_url + urlencode(new_params)
+    response = session.get(url, verify=False, timeout=10).content
+    alllist = json.loads(response)["res"]
+    print(alllist)
+    for i in range(len(alllist)):
+        data = alllist[i]["data"]
+        for x in data:
+            if x:
+                node_list.append(x["id"])
+    return node_list
 
 
 if __name__ == "__main__":
@@ -52,28 +60,29 @@ if __name__ == "__main__":
     recomm_code = os.environ['recomm_code']
     vemss_id = os.environ['vemss_id']
     vmess_api = os.environ['vmess_api']
+    vmess_api2 = os.environ['vmess_api2']
     headers = {
         'User-Agent': 'Dalvik/2.1.0 (Linux; U; Android 9.1.0; MI 9 MIUI/V9.5.6.0.OEACNFA)',
         'Connection': 'Keep-Alive',
         'Accept-Encoding': 'gzip'
     }
-    params = {'vip': 'false', 'proto': 'v4', 'platform': 'android', 'ver': '8.3.71125',
-              'deviceid': vemss_id, 'unicode': vemss_id, 't': str(round(datetime.now().timestamp() * 1000)),
-              'code': vmess_code, 'recomm_code': recomm_code, 'f': '2024-01-14',
-              'install': '2024-01-14', 'token': '',
-              'package': vmess_name, 'width': '362.72726', 'height': '770.1818'
-              }
-
+    session = requests.Session()
+    session.headers.clear()
+    session.headers.update(
+        headers
+    )
+    params = {'proto': 'v2', 'platform': 'android', 'ver': '4.1.12021', 'deviceid': vemss_id,
+              'unicode': vemss_id, 't': str(round(datetime.now().timestamp() * 1000)),
+              'code': vmess_code,
+              'recomm_code': '', 'f': '2023-09-11', 'install': '2023-09-11',
+              'token': vmess_token, 'package': vmess_name,
+              'isvip': 'true', 'unlimit': 'true', 'apps': '2a6a1fc7bd66c9643075846c2d77bdd2', 'area': '2'}
+    List = getlist()
     nodes = []
-    for i in range(20):
+    for i in List:
         try:
-            vmess_dict = json.loads(base64.b64decode(decrypt(get_vmess(), vemss_key)[8:]).decode())
-            try:
-                address = get_address(vmess_dict['add'])
-            except Exception as e:
-                print(e)
-                address = vmess_dict['add']
-            vmess_dict['ps'] = address + '|Github搜索TrojanLinks'
+            vmess_dict = json.loads(base64.b64decode(decrypt(get_vmess(i), vemss_key)[8:]).decode())
+            vmess_dict['ps'] = '|Github搜索TrojanLinks'
             nodes.append("vmess://" + base64.b64encode(json.dumps(vmess_dict).encode()).decode() + '\n')
         except Exception as e:
             print(e)
